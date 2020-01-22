3fc152b071722bc32ddbe11897cf9bc961d4358a
Wildrimak <finalwildrimak@gmail.com>
2020-01-22 18:05:33 -0300

    Pattern Adapter: ThreePinSocket created!

---
+++ src/br/com/wildrimak/structural/adapter/api_accessible_now/ThreePinSocket.java
@@ -0,0 +1,9 @@
+package br.com.wildrimak.structural.adapter.api_accessible_now;
+
+public class ThreePinSocket {
+
+	public void plugInThreePinSocket() {
+		System.out.println("Connected to Three-Pin Socket");
+	}
+
+}
