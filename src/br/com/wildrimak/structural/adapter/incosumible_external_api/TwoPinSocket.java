3cf8376fdba3dd96947768f863ba6d28efcd4998
Wildrimak <finalwildrimak@gmail.com>
2020-01-22 18:04:59 -0300

    Pattern Adapter: TwoPinSocket created!

---
+++ src/br/com/wildrimak/structural/adapter/incosumible_external_api/TwoPinSocket.java
@@ -0,0 +1,9 @@
+package br.com.wildrimak.structural.adapter.incosumible_external_api;
+
+public class TwoPinSocket {
+
+	public void plugInTwoPinSocket() {
+		System.out.println("Connected to Two-Pin Socket");
+	}
+
+}
